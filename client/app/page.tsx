b2938e0fc7bc918047e2cdd115a016ddb051418c
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2024-08-15 09:45:07 +0530

    00:00:00 - 00:35:16 Courses,Reviews,FAQ,Footer UI and features
    00:00:00 - 00:35:16 Courses,Reviews,FAQ,Footer UI and features

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,27 +3,40 @@ import React, { FC, useEffect, useState } from "react";
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
 import Hero from "./components/Route/Hero";
+import Courses from "./components/Route/Courses";
+import Reviews from "./components/Route/Reviews";
+import FAQ from "./components/FAQ/FAQ";
 import Footer from "./components/Footer";
 
-interface Props {}
+interface Props { }
 
 const Page: FC<Props> = (props) => {
-  const [open, setOpen] = useState(false); // Added state for `open`
-  const [activeItem, setActiveItem] = useState(0); // Added state for `activeItem`
-  const [route,setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
 
   return (
-    <div>
+    <>
       <Heading
-        title="ELearning - Admin"
+        title="ELearning"
         description="ELearning is a platform for students to learn and get help from teachers"
         keywords="Prograaming,MERN,Redux,Machine Learning"
       />
-      <Header open={open} setOpen={setOpen} activeItem={activeItem} setRoute={setRoute} route={route} />
-      <Hero/>
-      <Footer />
-    </div>
-
+      <div>
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+        <Hero />
+        <Courses />
+        <Reviews />
+        <FAQ />
+        <Footer />
+      </div>
+    </>
   );
 };
 
