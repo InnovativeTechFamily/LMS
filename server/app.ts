8df375df7b207c4afdb3cfe9c0e3002138fe8851
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2024-08-15 14:04:57 +0530

    done
    Done now start deploying than fix bugs

--- server/app.ts
+++ server/app.ts
@@ -10,7 +10,7 @@ import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
 import analyticsRouter from "./routes/analytics.route";
 import layoutRouter from "./routes/layout.route";
-
+import { rateLimit } from 'express-rate-limit'
 //body parser
 app.use(express.json({ limit: "50mb" }));
 
@@ -25,6 +25,15 @@ app.use(
     credentials:true
   })
 );
+
+// api requests limit
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
 //routes
 // routes
@@ -52,5 +61,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
-
+// middleware calls
+app.use(limiter);
 app.use(ErrorMiddleware);
